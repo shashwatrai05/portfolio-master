4978198b206127935d1c6712afffa88e9d5190c6
Shashwat Rai <shashwatrai575@gmail.com>
2024-10-31 01:39:09 +0530

    first commit

---
+++ app/routes/home/profile.jsx
@@ -0,0 +1,92 @@
+import profileImgLarge from '~/assets/profile-large.jpg';
+import profileImgPlaceholder from '~/assets/profile-placeholder.jpg';
+import profileImg from '~/assets/profile.jpg';
+import { Button } from '~/components/button';
+import { DecoderText } from '~/components/decoder-text';
+import { Divider } from '~/components/divider';
+import { Heading } from '~/components/heading';
+import { Image } from '~/components/image';
+import { Link } from '~/components/link';
+import { Section } from '~/components/section';
+import { Text } from '~/components/text';
+import { Transition } from '~/components/transition';
+import { Fragment, useState } from 'react';
+import { media } from '~/utils/style';
+import katakana from './katakana.svg';
+import styles from './profile.module.css';
+
+const ProfileText = ({ visible, titleId }) => (
+  <Fragment>
+    <Heading className={styles.title} data-visible={visible} level={3} id={titleId}>
+      <DecoderText text="Hi there" start={visible} delay={500} />
+    </Heading>
+    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    I'm Shashwat Rai, a B.Tech student in Computer Science and Engineering at KIET, Ghaziabad, with a passion for app development and design. Skilled in Flutter & Firebase, I create impactful, user-centered applications and engaging UI/UX designs. My journey includes roles as the Technical Lead at E-Cell KIET and App Development Captain at GDSC KIET, alongside hands-on experience from internships and hackathons. I’m dedicated to driving innovation, solving real-world problems, and bringing ideas to life through technology.
+    </Text>
+  </Fragment>
+);
+
+export const Profile = ({ id, visible, sectionRef }) => {
+  const [focused, setFocused] = useState(false);
+  const titleId = `${id}-title`;
+
+  return (
+    <Section
+      className={styles.profile}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
+      as="section"
+      id={id}
+      ref={sectionRef}
+      aria-labelledby={titleId}
+      tabIndex={-1}
+    >
+      <Transition in={visible || focused} timeout={0}>
+        {({ visible, nodeRef }) => (
+          <div className={styles.content} ref={nodeRef}>
+            <div className={styles.column}>
+              <ProfileText visible={visible} titleId={titleId} />
+              <Button
+                secondary
+                className={styles.button}
+                data-visible={visible}
+                href="/contact"
+                icon="send"
+              >
+                Send me a message
+              </Button>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.tag} aria-hidden>
+                <Divider
+                  notchWidth="64px"
+                  notchHeight="8px"
+                  collapsed={!visible}
+                  collapseDelay={1000}
+                />
+                <div className={styles.tagText} data-visible={visible}>
+                  About me
+                </div>
+              </div>
+              <div className={styles.image}>
+                <Image
+                  reveal
+                  delay={100}
+                  placeholder={profileImgPlaceholder}
+                  srcSet={`${profileImg} 480w, ${profileImgLarge} 960w`}
+                  width={960}
+                  height={1280}
+                  sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                  alt="Me smiling like a goofball at the Qwilr office in Sydney"
+                />
+                <svg className={styles.svg} data-visible={visible} viewBox="0 0 136 766">
+                  <use href={`${katakana}#katakana-profile`} />
+                </svg>
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </Section>
+  );
+};
